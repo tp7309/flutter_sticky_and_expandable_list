1ad598c976890bc202888080ce593ff4d35b48b4
tp7309 <yiyou7309@gmail.com>
2020-03-11 13:05:32 +0800

    update gif

--- example/lib/example_sliver.dart
+++ example/lib/example_sliver.dart
@@ -1,4 +1,4 @@
-import 'package:example/sample_data.dart';
+import 'sample_data.dart';
 import 'package:flutter/material.dart';
 import 'package:sticky_and_expandable_list/sticky_and_expandable_list.dart';
 
@@ -26,7 +26,7 @@ class _ExampleSliverState extends State<ExampleSliver> {
           SliverExpandableList(
             builder: SliverExpandableChildDelegate<String, Section>(
               sectionList: sectionList,
-              headerBuilder: (context, section, index) => _Header(section),
+              headerBuilder: _buildHeader,
               itemBuilder: (context, section, item, index) => ListTile(
                 leading: CircleAvatar(
                   child: Text("$index"),
@@ -39,23 +39,23 @@ class _ExampleSliverState extends State<ExampleSliver> {
       ),
     );
   }
-}
 
-class _Header extends StatelessWidget {
-  final Section section;
-
-  _Header(this.section);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        color: Colors.lightBlue,
-        height: 48,
-        padding: EdgeInsets.only(left: 20),
-        alignment: Alignment.centerLeft,
-        child: Text(
-          section.header,
-          style: TextStyle(color: Colors.white),
-        ));
+  Widget _buildHeader(BuildContext context, Section section, int index) {
+    return InkWell(
+        child: Container(
+            color: Colors.lightBlue,
+            height: 48,
+            padding: EdgeInsets.only(left: 20),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              section.header,
+              style: TextStyle(color: Colors.white),
+            )),
+        onTap: () {
+          //toggle section expand state
+          setState(() {
+            section.setSectionExpanded(!section.isSectionExpanded());
+          });
+        });
   }
 }
